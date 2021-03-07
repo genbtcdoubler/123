22219686a48a8b1f6921efb234a2920b8324a978
genbtcdoubler <79389495+genbtcdoubler@users.noreply.github.com>
2021-03-07 12:41:51 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$UserAgent = "Mozilla/5.0 (Linux; Android 6.0.1; Lenovo P1a42 Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.91 Mobile Safari/537.36";
+$AllCookie = "__cfduid=dff0551cfff1aa508373ef5f6abe122e11615139723;_ga=GA1.2.1541971118.1615139727;_luid=1514235;_gid=GA1.2.582866841.1615139727;_lrtk=QoBkA9pAZBsqRwWFeZEn02rLeTUKzfN8uXwW4L0lDvzLEqGjtOY0RmDw0Dnbwmrd";
+
+?>
